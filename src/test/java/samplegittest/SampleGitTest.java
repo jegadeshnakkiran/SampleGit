809b63f20bf84d96de9696f279381558fe1746c5
jegadeshnakkiran <jegadesh.nakkiran30@gmail.com>
2018-06-16 12:14:55 +0530

    full projected added

---
+++ src/test/java/samplegittest/SampleGitTest.java
@@ -0,0 +1,31 @@
+package samplegittest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SampleGitTest {
+	
+	@Test
+	public void logintest() throws IOException{
+		
+		WebDriver wd= new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.bing.com/");
+		File f=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(f, new File("E:\\Seleniumworkspace_new\\SampleGitTest\\src\\test\\resources\\screenshort\\bing.png"));
+		
+		
+		
+		
+		
+		
+	}
+
+}
